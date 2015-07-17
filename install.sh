afa007cd6883e2e15e052b4e8eb7f7bfaed43b77
vagrant <vagrant@localhost.localdomain>
2015-07-17 11:07:10 +0200

    Updated installer

--- install.sh
+++ install.sh
@@ -6,10 +6,9 @@ rm -rf ~/.vim
 if [ ! -d ~/.vim/bundle/vundle ]; then
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
-vim +PluginInstall! +qall > /dev/null 2>&1
+sudo vim +PluginInstall! +qall > /dev/null 2>&1
 
 . install-snippets.sh
 . install-vimrc.sh
 
-# overwrite tmux configuration
 cp -f $PWD/tmux.conf ~/.tmux.conf
