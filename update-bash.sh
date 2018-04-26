a06dbb328be61940d2998f4531517948c3621176
Simone Gentili <sensorario@gmail.com>
2018-04-26 16:15:50 +0200

    resolve #9 - ask committer during installation

--- update-bash.sh
+++ update-bash.sh
@@ -3,6 +3,8 @@ echo ""
 echo "Overriding bash aliases"
 
 cp -f $PWD/bash_aliases ~/.bash_aliases
+cp -f $PWD/bash_profile ~/.bash_profile
 cp -f $PWD/git-completion.bash ~/.git-completion.bash
 
 source ~/.bash_aliases
+source ~/.bash_profile
