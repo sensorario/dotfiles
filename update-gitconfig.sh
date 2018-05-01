79748ea929b1de108a07ed9afa962826ddf8668c
Simone Gentili <sensorario@gmail.com>
2018-05-01 06:34:33 +0200

    apply consistent format

--- update-gitconfig.sh
+++ update-gitconfig.sh
@@ -6,8 +6,10 @@ read committer_email
 sed "s/{{committer_user}}/$committer_user/g" <./gitconfig.dist >./temp
 sed "s/{{committer_email}}/$committer_email/g" <./temp >./gitconfig
 rm ./temp
+
 echo ""
 echo "Overriding gitconfig file"
+
 cp -f $PWD/gitconfig ~/.gitconfig
 rm ./gitconfig
 cp -f $PWD/gitignore ~/.gitignore;
