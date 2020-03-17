4cd22f5bfbd9f1b92fe7550557ac428c9025636a
Simone Gentili <sensorario@gmail.com>
2020-03-17 03:38:37 +0100

    Start zsh version of dotfiles due to Catalina update.

--- update-bash.sh
+++ update-bash.sh
@@ -3,8 +3,7 @@ echo ""
 echo "Overriding bash aliases"
 
 cp -f $PWD/bash_aliases ~/.bash_aliases
-cp -f $PWD/bash_profile ~/.bash_profile
-cp -f $PWD/git-completion.bash ~/.git-completion.bash
+cp -f $PWD/zshrc ~/.zshrc
 
 source ~/.bash_aliases
 source ~/.bash_profile
